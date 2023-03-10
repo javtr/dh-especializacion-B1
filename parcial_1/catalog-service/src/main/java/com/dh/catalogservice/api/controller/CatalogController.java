f4bfc7926d83493b0c7cff9dde2179a0e41fa6a7
javtr <javtriana.z@gmail.com>
2023-03-09 22:41:18 -0500

    gateway

--- parcial_1/catalog-service/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
+++ parcial_1/catalog-service/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
@@ -1,7 +1,10 @@
 package com.dh.catalogservice.api.controller;
 
+import com.dh.catalogservice.api.client.ImoviesClient;
 import com.dh.catalogservice.api.service.CatalogService;
 import com.dh.catalogservice.domain.model.Movie;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +17,13 @@ import java.util.List;
 @RequestMapping("/catalog")
 public class CatalogController {
 
-    private final CatalogService service;
-
-    public CatalogController(CatalogService service) {
-        this.service = service;
-    }
-
+    @Autowired
+    private ImoviesClient imoviesClient;
 
     @GetMapping("/{genre}")
-    ResponseEntity<List<Movie>> getGenre(@PathVariable String genre) {
-
-        return ResponseEntity.ok().body(service.getMovieByGenre(genre));
+    ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) {
+        return ResponseEntity.ok(imoviesClient.getMovieByGenre(genre));
     }
-
 }
+
+
--- parcial_1/catalog-service/src/main/java/com/dh/catalogservice/api/service/ICatalogService.java
+++ parcial_1/catalog-service/src/main/java/com/dh/catalogservice/api/service/ICatalogService.java
@@ -1,8 +1,10 @@
 package com.dh.catalogservice.api.service;
 
 import com.dh.catalogservice.domain.model.Movie;
+import org.springframework.cloud.openfeign.FeignClient;
 
 import java.util.List;
+
 
 public interface ICatalogService {
     List<Movie> getMovieByGenre (String genre);
