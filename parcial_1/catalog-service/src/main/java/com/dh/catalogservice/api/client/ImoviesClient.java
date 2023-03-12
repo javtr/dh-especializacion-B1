61b89a47d9a4ee2c8967853d854a6774462252c1
javtr <javtriana.z@gmail.com>
2023-03-11 23:06:05 -0500

    final

--- parcial_1/catalog-service/src/main/java/com/dh/catalogservice/api/client/ImoviesClient.java
+++ parcial_1/catalog-service/src/main/java/com/dh/catalogservice/api/client/ImoviesClient.java
@@ -1,6 +1,8 @@
 package com.dh.catalogservice.api.client;
 
+import com.dh.catalogservice.api.configuration.LoadBalancerConfiguration;
 import com.dh.catalogservice.domain.model.Movie;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import java.util.List;
 
 
 @FeignClient("movie-service")
+@LoadBalancerClient(name = "movie-service",configuration = LoadBalancerConfiguration.CustomLoadBalancerConfiguration.class)
 public interface ImoviesClient {
     @GetMapping("/movies/{genre}")
     List<Movie> getMovieByGenre(@PathVariable String genre);
